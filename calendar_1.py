739cdcdd2ef006ef134f3c3db97ff24899c820d3
sanjanakumar123 <sanjanaklav@gmail.com>
2026-07-12 17:54:28 -0600

    Add files via upload

---
+++ calendar_1.py
@@ -0,0 +1,13 @@
+import calendar
+
+for month in calendar.month_name[1:]:
+    print (month)
+
+#print(calendar.calendar(2026))
+#print(calendar.month(2026, 7))
+#print(calendar.isleap(2024))
+#print(calendar.weekday(2026, 7, 12))
+#print(calendar.monthrange(2026, 7))
+#print(calendar.leapdays(2000,2014))
+#print(calendar.month_name[1])
+#print(calendar.day_name[6])
